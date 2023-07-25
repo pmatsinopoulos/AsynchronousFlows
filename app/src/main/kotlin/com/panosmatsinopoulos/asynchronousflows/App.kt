1ffb34be3e6369b8743f217e15864889ca1d1d4f
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-25 12:34:45 +0300

    With Flow#combine we get data from both streams whenever they emit.

--- app/src/main/kotlin/com/panosmatsinopoulos/asynchronousflows/App.kt
+++ app/src/main/kotlin/com/panosmatsinopoulos/asynchronousflows/App.kt
@@ -5,9 +5,9 @@ package com.panosmatsinopoulos.asynchronousflows
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.runBlocking
 
 fun log(msg: String) {
@@ -20,7 +20,7 @@ fun main() {
         val nums = (1..3).asFlow().onEach { delay(300) }
         val strs = flowOf("one", "two", "three").onEach { delay(400) }
         val startTime = System.currentTimeMillis()
-        nums.zip(strs) { a, b -> "$a -> $b" }
+        nums.combine(strs) { a, b -> "$a -> $b" }
             .collect { value ->
                 log("collecting: $value at ${System.currentTimeMillis() - startTime} ms from start")
             }
